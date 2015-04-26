db835fa225ac97d0d83755c4f7f84072ca8a92db
acmegrid <acmeexpert@gmail.com>
2015-04-26 13:53:11 -0500

    all auto all the time 9

--- z/alert.js
+++ z/alert.js
@@ -1,4 +1,4 @@
 {
-"msg":"This a message from company Z. This is dynamic, see you there!",
+"msg":"This a message from company Z. You can easily change the formatting of selected text in the document text by choosing a look for the selected text from the Quick Styles gallery on the Home tab. You can also format text directly by using the other controls on the Home tab. Most controls offer a choice of using the look from the current theme or using a format that you specify directly. To change the overall look of your document, choose new Theme elements on the Page Layout tab. To change the looks available in the Quick Style gallery, use the Change Current Quick Style Set command. Both the Themes gallery and the Quick Styles gallery provide reset commands so that you can always restore the look of your document to the original contained in your current template.",
 "date":"4/1/2013"
 }
